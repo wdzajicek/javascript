1a1897114b068a562468d400854a4d0623f2e8f4
wdzajicek <tig.welder4@gmail.com>
2022-11-08 17:44:46 -0600

    Copy gh-pages build into `./docs` directory.

---
+++ docs/assets/js/src/main.js
@@ -0,0 +1,12 @@
+import '../../scss/main.scss';
+
+window.addEventListener('load', () => {
+  import('bootstrap/js/src/dropdown')
+    .then(({ default: Dropdown }) => Dropdown)
+    .then(Dropdown => {
+      import('bootstrap/js/src/collapse')
+        .then(({ default: Collapse }) => Collapse)
+    }).then(() => {
+      console.log(`Imported Collapse & Dropdown`);
+    })
+});
